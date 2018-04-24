3189d5c5948f630d924ba883ac396d33e4ba7203
Ben Hurst <BHurst8@gmail.com>
2018-04-24 12:55:28 -0700

    added backend server code

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
-// const client = new pg.Client(process.env.DATABASE_URL);
-// client.connect();
-// client.on('error', err => console.error(err));
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
 
 app.use(cors());
 
 app.get('/test', (req, res) => res.send('This is the root directory. Like rootbeer. Just no beer :('));
 
-// app.get('/api/v1/tasks', (req, res) => {
-//   client.query(`SELECT * from tasks;`)
-//   .then(results => res.send(results.rows))
-//   .catch(console.error);
-// });
+app.get('/api/v1/tasks', (req, res) => {
+  client.query(`SELECT * from books;`)
+  .then(results => res.send(results.rows))
+  .catch(console.error);
+});
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
